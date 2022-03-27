403923c07acf1228fff096d840892c533a21c5fa
AlexJHC <85096994+AlexJHC@users.noreply.github.com>
2022-03-27 10:10:24 +0300

    Revert "init project"
    This reverts commit 327698e418d5a5f74d365ac52a4ec482deecf8c7.

--- src/components/features/Loading/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import style from './Loading.module.css'
-
-export default function Loading() {
-  return (
-    <div className={style.container}>
-      <div className={style.ring}/>
-    </div>
-  )
-}
